b40b2264f2a0f3e247af061ad1664007257d8370
Yuliya <yuliya_saveleva@list.ru>
2022-11-03 15:27:08 +0500

    Add new info

---
+++ Task_29_Array8/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов с клавиатуры и выводит массив на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33
+
+int [] arr = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+  Console.Write ($"Введите элемент массива с индексом {i} ");
+  arr [i] = int.Parse(Console.ReadLine());
+}  
+
+void PrintArray (int []collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write ($"{collection[position]} ");
+        position++;
+    }
+}
+Console.WriteLine ();
+Console.Write ("Массив: [");
+PrintArray (arr);
+Console.WriteLine ("]");
+
+
+
